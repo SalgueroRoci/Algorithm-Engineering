c747cf9f39b5091a23d0cfa6077421412ac7a940
SalgueroRoci <salgueroroci@csu.fullerton.edu>
2017-01-19 17:28:53 -0800

    Update and rename ExhaustiveSearch.cpp to Project3/ExhaustiveSearch.cpp

--- Project3/ExhaustiveSearch.cpp
+++ Project3/ExhaustiveSearch.cpp
@@ -1,5 +1,5 @@
 // Assignment 3: Euclidean traveling salesperson problem: exhaustive optimization algorithm
-// XX YY ( YOU NEED TO COMPLETE YOUR NAME )
+//Rocio Salguero 
 // A special case of the classical traveling salesman problem (TSP) where the input is a Euclidean graph
 // INPUT: a positive integer n and a list P of n distinct points representing vertices of a Euclidean graph
 // OUTPUT: a list of n points from P representing a Hamiltonian cycle of minimum total weight for the graph.
